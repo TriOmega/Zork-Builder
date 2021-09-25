f37f01c26d66af23ec86a9ea7644206ddc5c798f
Travis Kosier <54700593+TravisKosier@users.noreply.github.com>
2021-09-25 17:53:22 -0400

    Zork 3.1
    Implemented look command. Rooms are now objects with a name and description property. Room description assignment is currently handled manually, to be improved in the next iteration.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -5,7 +5,7 @@ namespace Zork
 {
     class Program
     {
-        private static string CurrentRoom
+        private static Room CurrentRoom
         {
             get
             {
@@ -16,6 +16,8 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+
+            InitializeRoomDescriptions();
 
             Commands command = Commands.UNKNOWN;
             while (true)
@@ -28,30 +30,29 @@ namespace Zork
                     break;
                 }
 
-                string outputString;
                 switch (command)
                 {
                     case Commands.QUIT:
-                        outputString = "Thank you for playing.";
+                        Console.WriteLine("Thank you for playing.");
                         break;
 
                     case Commands.LOOK:
-                        outputString = "This is an open field west of a white house, with a boarded front door.\n\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        Console.WriteLine(CurrentRoom.Description);
+                        //outputString = "This is an open field west of a white house, with a boarded front door.\n\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
                         break;
 
                     case Commands.NORTH:
                     case Commands.SOUTH:
                     case Commands.EAST:
                     case Commands.WEST:
-                        outputString = Move(command) ? $"You moved {command}." : "The way is shut!";
+                        string outputString = Move(command) ? $"You moved {command}." : "The way is shut!";
+                        Console.WriteLine(outputString);
                         break;
 
                     default:
-                        outputString = "Unrecognized command";
+                        Console.WriteLine("Unrecognized command");
                         break;
                 }
-
-                Console.WriteLine(outputString);
             }
         }
 
@@ -83,11 +84,26 @@ namespace Zork
 
         private static bool IsDirection(Commands command) => Directions.Contains(command);
 
-        private static string[,] Rooms =
+        private static void InitializeRoomDescriptions()
         {
-            {"Rocky Trail", "South of House", "Canyon View"},
-            {"Forest","West of House","Behind House"},
-            {"Dense Woods", "North of House", "Clearing"}
+            Rooms[0,0].Description = "You are on a rock-strewn trail.";
+            Rooms[0,1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
+            Rooms[0,2].Description = "You are at the top of the Great Canyon on its south wall.";
+
+            Rooms[1,0].Description = "This is a forest, with trees in all directions around you.";
+            Rooms[1,1].Description = "This is an open field west of a white house, with a boarded front door.";
+            Rooms[1,2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";
+
+            Rooms[2,0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
+            Rooms[2,1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
+            Rooms[2,2].Description = "YOu are in a clearing, with a forest surrounding you on the west and south.";
+        }
+
+        private static readonly Room[,] Rooms =
+        {
+            { new Room("Rocky Trail"), new Room("South of House"),  new Room("Canyon View")},
+            { new Room("Forest"),      new Room("West of House"),   new Room("Behind House")},
+            { new Room("Dense Woods"), new Room("North of House"),  new Room("Clearing")}
         };
 
         private static readonly List<Commands> Directions = new List<Commands>
@@ -101,4 +117,3 @@ namespace Zork
         private static (int Row, int Column) Location = (1, 1);
     }
 }
-
