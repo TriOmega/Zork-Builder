0108290ec98b383a6a80e8d00c7927fbe6edb177
Travis Kosier <54700593+TravisKosier@users.noreply.github.com>
2021-10-04 00:01:41 -0400

    Zork 4.2
    Replaced custom file input reader with standardized JSON data serialization via Newtonsoft.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 
@@ -7,16 +8,6 @@ namespace Zork
     
     class Program
     {
-        private static readonly Dictionary<string, Room> RoomMap;
-
-        static Program()
-        {
-            RoomMap = new Dictionary<string, Room>();
-            foreach (Room room in Rooms)
-            {
-                RoomMap[room.Name] = room;
-            }
-        }
         
         private static Room CurrentRoom
         {
@@ -28,9 +19,9 @@ namespace Zork
 
         static void Main(string[] args)
         {
-            string defaultRoomsFilename = "Rooms.txt";
+            string defaultRoomsFilename = "Rooms.json";
             string roomsFilename = (args.Length > 0 ? args[(int)CommandLineArguments.RoomsFilename] : defaultRoomsFilename);
-            InitializeRoomDescriptions(roomsFilename);
+            InitializeRooms(roomsFilename);
 
             Console.WriteLine("Welcome to Zork!");
 
@@ -108,29 +99,9 @@ namespace Zork
 
         private static bool IsDirection(Commands command) => Directions.Contains(command);
 
-        private static void InitializeRoomDescriptions(string roomsFilename)
-        {
-            const string fieldDelimiter = "##";
-            const int expectedFieldCount = 2;
+        private static void InitializeRooms(string roomsFilename) => Rooms = JsonConvert.DeserializeObject<Room[,]>(File.ReadAllText(roomsFilename));
 
-            string[] lines = File.ReadAllLines(roomsFilename);
-            foreach (string line in lines)
-            {
-                string[] fields = line.Split(fieldDelimiter);
-                if (fields.Length != expectedFieldCount)
-                {
-                    throw new InvalidDataException("Invalid record.");
-                }
-
-                string name = fields[(int)Fields.Name];
-                string description = fields[(int)Fields.Description];
-
-                RoomMap[name].Description = description;
-            }
-
-        }
-
-        private static readonly Room[,] Rooms =
+        private static Room[,] Rooms =
         {
             { new Room("Rocky Trail"), new Room("South of House"),  new Room("Canyon View")},
             { new Room("Forest"),      new Room("West of House"),   new Room("Behind House")},
